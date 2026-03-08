4430d6025fed93cedc7f218cb1ca0f954cd63db1
mohghaderi <mohghaderi@gmail.com>
2026-03-07 20:17:20 -0600

    chore: include pending config updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
 	plugins: [react(), cloudflare()],
+	server: {
+		allowedHosts: ["remotepc1.xelead.com"],
+	},
 	resolve: {
 		alias: {
 			"@src": fileURLToPath(new URL("./src", import.meta.url)),
