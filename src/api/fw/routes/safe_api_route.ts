40bb3caa854fdc0ca777dfcc16bc825620e52d37
mohghaderi <mohghaderi@gmail.com>
2026-03-07 23:49:34 -0600

    add safe api route base class for auto wrapped handlers

---
+++ src/api/fw/routes/safe_api_route.ts
@@ -0,0 +1,34 @@
+import type { Context } from "hono";
+
+import type { IApp } from "@src/api/fw/api_app_types.ts";
+import { safeApi } from "@src/api/fw/routes/safe_api.ts";
+import type { IApiRequestContext, IApiResult } from "@src/interfaces/route_types.ts";
+
+type ApiAction<T> = (
+	c: Context<{ Bindings: Env }>,
+	requestContext: IApiRequestContext,
+) => Promise<IApiResult<T>> | IApiResult<T>;
+
+export abstract class SafeApiRouteModule {
+	private app: IApp;
+
+	constructor(app: IApp) {
+		this.app = app;
+	}
+
+	protected get<T>(path: string, action: ApiAction<T>) {
+		this.app.get(path, (c) => safeApi(c, (request_context) => action(c, request_context)));
+	}
+
+	protected post<T>(path: string, action: ApiAction<T>) {
+		this.app.post(path, (c) => safeApi(c, (request_context) => action(c, request_context)));
+	}
+
+	protected patch<T>(path: string, action: ApiAction<T>) {
+		this.app.patch(path, (c) => safeApi(c, (request_context) => action(c, request_context)));
+	}
+
+	protected delete<T>(path: string, action: ApiAction<T>) {
+		this.app.delete(path, (c) => safeApi(c, (request_context) => action(c, request_context)));
+	}
+}
--- src/api/modules/projects/project_api_routes.ts
+++ src/api/modules/projects/project_api_routes.ts
@@ -1,15 +1,12 @@
 import { IApp } from "@src/api/fw/api_app_types.ts";
-import { safeApi } from "@src/api/fw/routes/safe_api.ts";
+import { SafeApiRouteModule } from "@src/api/fw/routes/safe_api_route.ts";
 import projectsGet from "@src/api/modules/projects/actions/projects_get.ts";
 
-export class ProjectApiRoutes {
-	private app: IApp;
-
+export class ProjectApiRoutes extends SafeApiRouteModule {
 	constructor(app: IApp) {
-		this.app = app;
+		super(app);
 	}
 	register_routes() {
-		const app = this.app;
-		app.get("/api/projects", (c) => safeApi(c, (request_context) => projectsGet(request_context)));
+		this.get("/api/projects", (_c, request_context) => projectsGet(request_context));
 	}
 }
