8a144bfdf730fb10d1334c2fc19147641cad41ff
mohghaderi <mohghaderi@gmail.com>
2026-03-12 15:45:07 +0000

    change dev server port to 9010

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,40 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig({
-	plugins: [react(), cloudflare()],
-	cacheDir: "node_modules/.vite_cf_dev",
-	optimizeDeps: {
-		exclude: ["mongodb"],
-		include: ["punycode", "punycode/", "tr46", "whatwg-url"],
-	},
-	ssr: {
-		external: ["mongodb"],
+const useWranglerPlugin = process.env.USE_WRANGLER === "true";
+
+export default defineConfig(async () => {
+	const plugins = [react()];
+
+	if (useWranglerPlugin) {
+		const { cloudflare } = await import("@cloudflare/vite-plugin");
+		plugins.push(cloudflare());
+	}
+
+	return {
+		plugins,
+		cacheDir: "node_modules/.vite_cf_dev",
 		optimizeDeps: {
 			exclude: ["mongodb"],
 			include: ["punycode", "punycode/", "tr46", "whatwg-url"],
 		},
-	},
-	server: {
-		allowedHosts: ["remotepc1.xelead.com"],
-	},
-	resolve: {
-		alias: {
-			"@src": fileURLToPath(new URL("./src", import.meta.url)),
-			"punycode/": "punycode/punycode.js",
+		ssr: {
+			external: ["mongodb"],
+			optimizeDeps: {
+				exclude: ["mongodb"],
+				include: ["punycode", "punycode/", "tr46", "whatwg-url"],
+			},
 		},
-	},
+		server: {
+			port: 9010,
+			allowedHosts: ["remotepc1.xelead.com"],
+		},
+		resolve: {
+			alias: {
+				"@src": fileURLToPath(new URL("./src", import.meta.url)),
+				"punycode/": "punycode/punycode.js",
+			},
+		},
+	};
 });
